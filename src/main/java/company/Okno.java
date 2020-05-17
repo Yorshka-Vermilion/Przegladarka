496ce82a1143a9339c881ed7088b4a8f51052049
Yorshka <MangDaal@live.com>
2020-05-18 00:16:56 +0200

    yd

--- src/main/java/company/Okno.java
+++ src/main/java/company/Okno.java
@@ -7,22 +7,56 @@ import java.awt.event.ActionListener;
 
 public class Okno implements ActionListener{
     JFrame frame;
+    JPanel panel;
+    JPanel panel2;
+    JPanel spod;
     PasekAdresu pasekAdresu;
     public Okno(){
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int width = gd.getDisplayMode().getWidth();
+        int height = gd.getDisplayMode().getHeight();
+
+
         frame = new JFrame("Przeglada");
+        frame.setSize(width,height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER));
         // Dodanie pasku adresu
-        pasekAdresu = new PasekAdresu(frame.getWidth());
+        pasekAdresu = new PasekAdresu(width);
         pasekAdresu.addActionListener(this);
-        frame.add(pasekAdresu);
 
+
+        spod = new JPanel();
+        spod.setPreferredSize(new Dimension(width, height));
+        spod.setBackground(Color.BLUE);
+        spod.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(width, 40));
+        panel.setBounds(0,0,width,40);
+        panel.setBackground(Color.RED);
+
+
+        panel2 = new JPanel();
+        panel2.setPreferredSize(new Dimension(width, height-40));
+        panel2.setBackground(Color.GRAY);
+
+
+
+        frame.add(spod);
+        spod.add(panel);
+        panel.add(pasekAdresu);
+
+        spod.add(panel2);
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent event){
-        if(event.getSource() == pasekAdresu){
+        if(event.getSource() == pasekAdresu && pasekAdresu.focused){
             pasekAdresu.zapiszPole();
             System.out.println(pasekAdresu.wezPole());
         }
--- src/main/java/company/PasekAdresu.java
+++ src/main/java/company/PasekAdresu.java
@@ -1,15 +1,20 @@
 package company;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 
 
-public class PasekAdresu extends JTextField{
+public class PasekAdresu extends JTextField implements FocusListener {
     String poleTekstowe;
+    public boolean focused;
     public PasekAdresu(int rozmiar){
-        setSize(rozmiar,30);
+        setPreferredSize(new Dimension(rozmiar, 30));
         setEditable(true);
+        addFocusListener(this);
     }
 
     public void zapiszPole(){
@@ -17,6 +22,16 @@ public class PasekAdresu extends JTextField{
     }
     public String wezPole(){
         return this.poleTekstowe;
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        this.focused = true;
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        this.focused = false;
     }
 }
 
