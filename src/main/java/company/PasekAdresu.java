605cf46a1b97d96807bbf919713b7d11691f29cb
Yorshka <MangDaal@live.com>
2020-05-17 21:11:58 +0200

    v2

--- src/main/java/company/PasekAdresu.java
+++ src/main/java/company/PasekAdresu.java
@@ -1,32 +1,30 @@
 package company;
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.security.Key;
 
 
 public class PasekAdresu {
     public static int number = 0;
     public String addressHistory[] = new String[1024];
-    JTextField addressBar;
+    JTextField poleTekstowe;
 
     public PasekAdresu(JFrame frame){
-        addressBar = new JTextField("");
-        frame.add(addressBar);
-        addressBar.setSize(frame.getWidth(),30);
-        addressBar.setEditable(true);
+        poleTekstowe = new JTextField("");
+        frame.add(poleTekstowe);
+        poleTekstowe.setSize(frame.getWidth(),30);
+        poleTekstowe.setEditable(true);
 
         KeyStroke enter = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0);
-        addressBar.registerKeyboardAction(EnterEvent,enter,JComponent.WHEN_FOCUSED);
+        poleTekstowe.registerKeyboardAction(EnterEvent,enter,JComponent.WHEN_FOCUSED);
 
     }
 
     ActionListener EnterEvent = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent event) {
-            addressHistory[number++] = addressBar.getText();
+            addressHistory[number++] = poleTekstowe.getText();
         }
     };
 
