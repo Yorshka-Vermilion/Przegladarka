00e54f6ae2b263d71d4862d6110ffb02ea3ed914
Yorshka <MangDaal@live.com>
2020-05-17 22:41:55 +0200

    V2

--- src/main/java/company/Okno.java
+++ src/main/java/company/Okno.java
@@ -1,18 +1,34 @@
 package company;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-
-public class Okno {
+public class Okno implements ActionListener{
     JFrame frame;
-    Container WebMap;
+    PasekAdresu pasekAdresu;
     public Okno(){
         frame = new JFrame("Przeglada");
-        frame.setSize(800,600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-        PasekAdresu addressBar = new PasekAdresu(frame);
+        // Dodanie pasku adresu
+        pasekAdresu = new PasekAdresu(frame.getWidth());
+        pasekAdresu.addActionListener(this);
+        frame.add(pasekAdresu);
+
     }
+
+    public void actionPerformed(ActionEvent event){
+        if(event.getSource() == pasekAdresu){
+            pasekAdresu.zapiszPole();
+            System.out.println(pasekAdresu.wezPole());
+        }
+    }
+
+
+
+
 }
--- src/main/java/company/PasekAdresu.java
+++ src/main/java/company/PasekAdresu.java
@@ -5,28 +5,18 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 
-public class PasekAdresu {
-    public static int number = 0;
-    public String addressHistory[] = new String[1024];
-    JTextField poleTekstowe;
-
-    public PasekAdresu(JFrame frame){
-        poleTekstowe = new JTextField("");
-        frame.add(poleTekstowe);
-        poleTekstowe.setSize(frame.getWidth(),30);
-        poleTekstowe.setEditable(true);
-
-        KeyStroke enter = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0);
-        poleTekstowe.registerKeyboardAction(EnterEvent,enter,JComponent.WHEN_FOCUSED);
-
+public class PasekAdresu extends JTextField{
+    String poleTekstowe;
+    public PasekAdresu(int rozmiar){
+        setSize(rozmiar,30);
+        setEditable(true);
     }
 
-    ActionListener EnterEvent = new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent event) {
-            addressHistory[number++] = poleTekstowe.getText();
-        }
-    };
-
+    public void zapiszPole(){
+        poleTekstowe = getText();
+    }
+    public String wezPole(){
+        return this.poleTekstowe;
+    }
 }
 
