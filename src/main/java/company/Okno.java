dc54a713c152eae67e215cb1310893c0fa66a86d
Yorshka <MangDaal@live.com>
2020-05-18 17:28:28 +0200

    Obsługa kart

--- src/main/java/company/Okno.java
+++ src/main/java/company/Okno.java
@@ -1,22 +1,26 @@
 package company;
+
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
 import javafx.scene.web.WebView;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-public class Okno implements ActionListener{
+public class Okno implements ActionListener {
     JFrame frame;
     Panel spod, panel, panel2;
     PasekAdresu pasekAdresu;
     WebView webView;
     JFXPanel jfxPanel;
+    JPanel pustyPanel;
+    JTabbedPane tabbedPane;
+    boolean flaga;
     public Okno(){
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         int width = gd.getDisplayMode().getWidth();
@@ -29,6 +33,7 @@ public class Okno implements ActionListener{
         frame.setLayout(new FlowLayout(FlowLayout.CENTER));
 
         zainicjujLayout(width, height); // Inicjacja layoutu przeglądarki
+        flaga = true;
 
         frame.pack();
         frame.setVisible(true);
@@ -41,6 +46,8 @@ public class Okno implements ActionListener{
 
             wyszukaj(pasekAdresu.wezPole());
 
+        }else if(event.getSource() == webView){
+            System.out.println("ELO");
         }
     }
 
@@ -54,8 +61,9 @@ public class Okno implements ActionListener{
         panel = new Panel(width, 40, Color.RED, false);
         panel2 = new Panel(width, height-40, Color.GRAY, false);
 
+
         jfxPanel = new JFXPanel();
-        panel2.add(jfxPanel);
+        //panel2.add(jfxPanel);
 
         Platform.runLater(() -> {
             webView = new WebView();
@@ -63,6 +71,26 @@ public class Okno implements ActionListener{
             jfxPanel.setScene(new Scene(webView));
             webView.getEngine().load("http://www.google.com/");
         });
+
+        // Inizjalizacja kart
+        tabbedPane = new JTabbedPane();
+        tabbedPane.add(jfxPanel);
+        nowaKarta(width,height-40);
+
+        tabbedPane.addChangeListener(new ChangeListener(){
+            public void stateChanged(ChangeEvent e) {
+                if(tabbedPane.getSelectedIndex() == tabbedPane.getTabCount()-1){
+                    nowaKarta(width, height-40);
+                    tabbedPane.setSelectedIndex(tabbedPane.getTabCount()-2);
+                    System.out.println("Dodaje karte");
+                    return;
+                }
+            }
+        });
+        // Koniec kart
+
+
+        panel2.add(tabbedPane);
 
         // Zagnieżdzenie paneli
         frame.add(spod);
@@ -83,18 +111,28 @@ public class Okno implements ActionListener{
         } else {
             wyszukajWGoogle(url);
         }
-    }
+    } // Funkcja sprawdza url i wykonuje odpowiednią operacje zaladowania strony
 
     void zaladuj(String url){
         Platform.runLater(() -> {
             webView.getEngine().load(url);
         });
-    }
+    } // Laduje stronę o podanym url
 
-    void wyszukajWGoogle(String url){
+    void wyszukajWGoogle(String fraza){
         Platform.runLater(() -> {
-            webView.getEngine().load("http://google.com/search?q=" + url);
+            webView.getEngine().load("http://google.com/search?q=" + fraza);
         });
-    }
+    } // Szuka fraze w gooogle
 
+    void nowaKarta(int width, int height){
+        JFXPanel nowyJFX = new JFXPanel();
+        Platform.runLater(() -> {
+            WebView nowyWV = new WebView();
+            nowyJFX.setPreferredSize(new Dimension(width,height));
+            nowyJFX.setScene(new Scene(nowyWV));
+            nowyWV.getEngine().load("http://www.google.com/");
+        });
+        tabbedPane.add(nowyJFX);
+    }
 }
